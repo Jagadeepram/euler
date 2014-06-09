056ec7b55e0e4f5463dca4f6271edd782becfffa
Jagadeepram <connectjagadeep@gmail.com>
2014-06-09 20:32:40 +0200

    Solved Problem 4

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include "Problem1.h"
 #include "Problem2.h"
 #include "Problem3.h"
+#include "Problem4.h"
 using namespace std;
 
 int main()
 {
-    Problem3::Instance()->run();
+    Problem4::Instance()->run();
     return 0;
 }
